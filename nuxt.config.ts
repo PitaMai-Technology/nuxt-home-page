8a6a977e1dfe3485a0826766758da1b258eb7abb
Eita <kusaiyuka@gmail.com>
2025-10-17 20:55:02 +0900

    feat: Gtagを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/ui',
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "nuxt-gtag",
   ],
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
-  runtimeConfig: {
-    public: {
-      siteUrl: 'https://pitamai.com' // 実運用では環境変数で切り替える
-    }
-  }
-})
+  gtag: {
+    id: "G-YMJHQ4TNQH",
+  },
+});
