a574703f0d5c1d69996bc7a3ed82cd1a323c7154
Eita <kusaiyuka@gmail.com>
2025-10-27 07:10:07 +0900

    feat: FrontHeroコンポーネントにリンクを追加し、recruitページのタイトルを変更

--- app/components/global/FrontHero.vue
+++ app/components/global/FrontHero.vue
@@ -1,9 +1,27 @@
+<script setup lang="ts">
+const links = ref([
+  {
+    label: '参加してみる',
+    to: 'https://discord.gg/szxdR8jZbT',
+    icon: 'i-skill-icons-discord',
+    trailingIcon: 'i-lucide-arrow-right',
+    target: '_blank'
+  },
+  {
+    label: '詳しく見てみる',
+    to: '/about',
+    color: 'neutral',
+    trailingIcon: 'i-lucide-arrow-right'
+  }
+])
+</script>
 <template>
   <UPageHero
     title="ちょうど良いぐらいのテクノロジー"
     description="ちょうど良いぐらいのテクノロジーで、チームでOSSや創作活動をする。"
     headline="PitaMai Technology / ピタマイ・テクノロジー"
     class="bg-primary-400/10 rounded-xl"
+    :links="links"
   />
 </template>
 
