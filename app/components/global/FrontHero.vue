65998f2f82d987821d80db813b92af978cd46575
Eita <kusaiyuka@gmail.com>
2025-12-16 08:54:13 +0900

    docs(リクルート): メンバー募集の説明を修正し、プロジェクトの詳細を整理 fix: 任意団体という旨を強調

--- app/components/global/FrontHero.vue
+++ app/components/global/FrontHero.vue
@@ -16,13 +16,8 @@ const links = ref([
 ])
 </script>
 <template>
-  <UPageHero
-    title="ちょうど良いぐらいのテクノロジー"
-    description="ちょうど良いぐらいのテクノロジーで、チームでOSSや創作活動をする。"
-    headline="PitaMai Technology / ピタマイ・テクノロジー"
-    class="bg-primary-400/10 rounded-xl"
-    :links="links"
-  />
+  <UPageHero title="ちょうど良いぐらいのテクノロジー" description="ちょうど良いぐらいのテクノロジーで、チームでOSSや創作活動をする。(当団体は任意団体(サークル)です)"
+    headline="PitaMai Technology / ピタマイ・テクノロジー" class="bg-primary-400/10 rounded-xl" :links="links" />
 </template>
 
 <style scoped></style>
